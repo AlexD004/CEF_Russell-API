35387e1261db9c880c01f493ca7a4cce1e490141
AlexD004 <dupre.alexandre04@gmail.com>
2025-03-10 15:36:58 +0100

    add form on index page and simple redirection to dashboard

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
+const { checkJWT } = require('../middlewares/private')
+
 const userRouter = require('../routes/users')
 const catwaysRouter = require('../routes/catways');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Russell Réservation' });
+});
+
+/* GET dashboard. */
+router.get('/dashboard',checkJWT, function(req, res, next) {
+  res.render('dashboard', { title: 'Tableau de bord' });
 });
 
 router.use('/users', userRouter);
--- services/users.js
+++ services/users.js
@@ -126,7 +126,8 @@ exports.authenticate = async (req, res, next) => {
           });
 
           res.header('Authorization', 'Bearer ' + token);
-          return res.status(200).json('authenticate_succeed');
+          //return res.status(200).json('authenticate_succeed');
+          return res.status(200).redirect('/dashboard');
         }
 
         return res.status(403).json('Wrong Credentials');
