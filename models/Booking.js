c1f0282265183fc37e5f3e4ad08b96bcfcab5dd1
AlexD004 <dupre.alexandre04@gmail.com>
2025-03-17 13:51:05 +0100

    add booking model

---
+++ models/Booking.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema;
+
+const Booking = new Schema({
+    bookingID:{
+        type        : Number
+    },
+    catwayNumber: {
+        type        : Number,
+        required    : [true, 'Le numéro de catway est requis']
+    },
+    clientName: {
+        type        : String,
+        trim        : true,
+        required    : [true, 'Le nom du client est requis']
+    },
+    boatName: {
+        type        : String,
+        trim        : true,
+        required    : [true, 'Le nom du bateau est requis']
+    },
+    checkIn: {
+        type        : Date,
+        required    : [true, 'La date de début de réservation est requise']
+    },
+    checkOut: {
+        type        : Date,
+        required    : [true, 'La date de fin de réservation est requise']
+    }
+}, {
+    timestamps      : true
+});
+
+module.exports = mongoose.model('Catway', Catway) ;
